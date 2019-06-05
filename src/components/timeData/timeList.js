35afb342ed9d340dd2f85f1b3c7dd0363ce3237a
jistjoalal <jistjoalal@gmail.com>
2019-06-05 05:22:14 -0700

    refactor timelist as functional component

--- src/components/timeData/timeList.js
+++ src/components/timeData/timeList.js
@@ -6,31 +6,26 @@ import Time from "../Time";
 
 import "./TimeList.css";
 
-export default class TimeList extends React.Component {
-  render() {
-    const { times } = this.props;
-    return (
-      <div>
-        <h2>Times:</h2>
-        <button onClick={actions.removeAllTimes}>Clear</button>
-        <div className="timeList">
-          <table>
-            <tbody>
-              {times.map((time, i) => (
-                <tr key={time.savedAt}>
-                  <td>{times.length - i}</td>
-                  <td>
-                    <Time time={time.time} />
-                  </td>
-                  <td>
-                    <button onClick={() => actions.removeTime(time)}>X</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
-}
+export default ({ times }) => (
+  <div>
+    <h2>Times:</h2>
+    <button onClick={actions.removeAllTimes}>Clear</button>
+    <div className="timeList">
+      <table>
+        <tbody>
+          {times.map((time, i) => (
+            <tr key={time.savedAt}>
+              <td>{times.length - i}</td>
+              <td>
+                <Time time={time.time} />
+              </td>
+              <td>
+                <button onClick={() => actions.removeTime(time)}>X</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+);
